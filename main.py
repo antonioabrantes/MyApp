fb77c9849f10312d4d755ef20f8a64b15693dfb1
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-09-15 20:57:59 -0300

    aa

--- main.py
+++ main.py
@@ -1,42 +1,8 @@
-import streamlit as st
-import time
-import pandas as pd
+from flask import Flask, render_template
 
-def main():
-    st.title("Título Novo")
-    st.write("alguns exemplos de uso")
+app = Flask(__name__)
 
-    st.header("Input de texto")
-    texto = st.text_input("Digite aqui:")
-    if texto:
-        st.write("Texto: ",texto)
-
-    st.header("Selectbox")
-    texto = st.selectbox("Selecione:",['op1','op2','op3'])
-    if texto:
-        st.write("Opção: ",texto)
-
-    st.header("Checkbox")
-    valor = st.checkbox("Marque:")
-    st.write("Checkbox ativado: ",valor)
-
-    st.header("Botão")
-    if st.button("Clique aqui"):
-        st.write("você clicou no botão")
-
-    st.header("loading...")
-    with st.spinner("Aguarde.."):
-        time.sleep(3)
-        st.write("encerrado")
-        st.success("carregado com sucesso")
-
-    st.header("upload de arquivo")
-    arquivo = st.file_uploader("Escolha arquivo", type=['pdf','txt'])
-    if arquivo:
-        st.write("Nome do arquivo: ",arquivo.name)
-
-    df = pd.read_csv("my_data.csv")
-    st.line_chart(df)
-
-
-main()
+@app.route('/')
+def hello_world():
+    name = "Ola mundo tudo bem !"
+    return render_template('index.html',name=name)
