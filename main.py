6a7975458bf4487ce275b0fa1bb9851ef578f3a1
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-09-15 22:00:00 -0300

    00

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-   
-import os
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    name = "Ola mundo"
-    return render_template('index.html',name=name)
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Pegue a porta da variável de ambiente ou use 5000
-    app.run(host="0.0.0.0", port=port) 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
