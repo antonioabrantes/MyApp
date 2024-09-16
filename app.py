18f9a3035c04bf112cf4f3ff0cf9bdb7f479de7d
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-09-15 22:38:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+#https://github.com/templates-back4app/containers-python-flask-sample
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Back4apper!"
+    return "ola mundo!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
