e85d4df85ccf9c5ac430680952bffcbc26923172
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-09-15 21:33:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+   
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    name = "Ola mundo tudo bem !"
+@app.route("/")
+def hello():
+    name = "Ola mundo"
     return render_template('index.html',name=name)
+
+if __name__ == "__main__":
+    # Defina a porta a partir da variável de ambiente PORT, ou use 5000 por padrão
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
